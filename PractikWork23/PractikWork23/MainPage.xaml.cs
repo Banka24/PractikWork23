67411ba0b354354a4d3c233241140d03194ad542
Banka24 <Aq567489012@gmail.com>
2023-12-21 17:56:31 +0300

    Добавьте файлы проекта.

---
+++ PractikWork23/PractikWork23/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PractikWork23
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void OkButton_Clicked(object sender, EventArgs e)
+        {
+            AnswerEntry.Text = "";
+            double x, y;
+            if(!double.TryParse(OXEntry.Text, out x))
+            {
+                OXEntry.Text += "Введите число";
+            }
+            if (!double.TryParse(OYEntry.Text, out y))
+            {
+                OYEntry.Text += "Введите число";
+            }
+
+            AnswerEntry.Text += y < 23 && x < 0 && y > -x ? "ДА" : "НЕТ";
+           
+        }
+
+        private void CancelButton_Clicked(object sender, EventArgs e)
+        {
+            OXEntry.Text = "";
+            OYEntry.Text = "";
+            AnswerEntry.Text = "";
+        }
+
+    }
+}
